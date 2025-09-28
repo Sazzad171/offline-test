256a1109373d0fdbe4446b59d31f744323312adf
Sazzad Bin Jafor <sazzad.jafor@technonext.com>
2025-09-28 14:25:19 +0600

    update

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -2,12 +2,22 @@ import Link from "next/link";
 
 function Header() {
   return (
-    <header className="p-4 bg-blue-600 text-white text-center">
-      <nav className="flex gap-4 justify-center">
-        <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
-      </nav>
-    </header>
+    <nav className="flex gap-6 mt-2 sm:mt-0 text-center">
+      <Link 
+        href="/" 
+        className="text-white hover:text-blue-200 transition-colors duration-200"
+        style={{fontSize: "20px", padding: "20px"}}
+      >
+        Home
+      </Link>
+      <Link 
+        href="/about" 
+        className="text-white hover:text-blue-200 transition-colors duration-200"
+        style={{fontSize: "20px", padding: "20px"}}
+      >
+        About
+      </Link>
+    </nav>
   );
 }
 
